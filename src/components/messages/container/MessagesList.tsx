602c6928a2db9769fd7d8ce186318ef925f9595a
Jordy Bukonda Tshibangu <69379641+JordyBukondaTshibangu@users.noreply.github.com>
2024-03-09 13:44:26 +0200

    fix : components simplify

--- src/components/messages/container/MessagesList.tsx
+++ src/components/messages/container/MessagesList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ChatPreview from "../regular/ChatPreview";
 import UserPreview from "../regular/UserPreview";
 
@@ -27,6 +27,22 @@ type MessagesListProps = {
 };
 
 const MessagesList: React.FC<MessagesListProps> = ({ chats, selectChat }) => {
+  const [chatList, setChatList] = useState<ChatT[]>(chats);
+
+  const handleSearchChat = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value) {
+      let updatedChatList: ChatT[] = chatList;
+      updatedChatList = updatedChatList.filter((chat) =>
+        chat.sender
+          .toLocaleLowerCase()
+          .includes(event.target.value.toLocaleLowerCase()),
+      );
+      setChatList(updatedChatList);
+    } else {
+      // console.log("first");
+      setChatList(chats);
+    }
+  };
   return (
     <div className="lg:sticky top-32 w-full lg:w-[40%] lg:bg-white  lg:dark:bg-darkHeader lg:rounded-lg lg:px-5 lg:py-10 flex flex-col gap-12 h-fit">
       <div className="w-full hidden lg:flex">
@@ -40,9 +56,10 @@ const MessagesList: React.FC<MessagesListProps> = ({ chats, selectChat }) => {
         type="text"
         className="w-full h-12 rounded-full px-5 py-2 text-lightFontColor dark:text-fontColor border dark:border-none border-solid border-grayFour lg:bg-light dark:bg-darkHeader lg:dark:bg-dark outline-none"
         placeholder="Search..."
+        onChange={handleSearchChat}
       />
       <div className="flex flex-col gap-5 overflow-y-auto max-h-[60vh]">
-        {chats.map((chat: any, index: number) => (
+        {chatList.map((chat: any, index: number) => (
           <ChatPreview key={index} chat={chat} selectChat={selectChat} />
         ))}
       </div>
